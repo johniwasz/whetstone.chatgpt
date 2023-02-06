bb7b58516055e4ee454a404b4df3f7531e4724be
johniwasz <johniwasz@users.noreply.github.com>
2023-02-06 22:20:03 +0000

    Deploying to gh-pages from  @ 1651f4bb367bdf6f407726ed2b3ef89658b40390 🚀

---
+++ js/TableGenerator.js
@@ -0,0 +1,16 @@
+﻿export function BlazorDownloadFile(filename, contentType, content) {
+    const file = new File([content], filename, { type: contentType });
+    const exportUrl = URL.createObjectURL(file);
+
+    // Create the <a> element and click on it
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+
+    // We don't need to keep the object URL, let's release the memory
+    // On older versions of Safari, it seems you need to comment this line...
+    URL.revokeObjectURL(exportUrl);
+}
