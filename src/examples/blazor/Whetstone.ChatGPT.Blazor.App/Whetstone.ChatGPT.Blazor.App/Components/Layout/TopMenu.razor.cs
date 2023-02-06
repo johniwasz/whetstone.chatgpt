3e024cf999448bf0377441270797b818d3b35522
John Iwasz <iwaszj@gmail.com>
2023-02-05 19:06:21 -0500

    Added missing file

---
+++ src/examples/blazor/Whetstone.ChatGPT.Blazor.App/Whetstone.ChatGPT.Blazor.App/Components/Layout/TopMenu.razor.cs
@@ -0,0 +1,28 @@
+﻿using Blazorise.Localization;
+using Microsoft.AspNetCore.Components;
+
+namespace Whetstone.ChatGPT.Blazor.App.Components.Layout
+{
+    public partial class TopMenu
+    {
+
+        [Inject]
+        protected ITextLocalizerService LocalizationService { get; set; } = default!;
+
+        private bool topbarVisible = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await SelectCulture("en-US");
+
+            await base.OnInitializedAsync();
+        }
+
+        Task SelectCulture(string name)
+        {
+            LocalizationService.ChangeLanguage(name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
